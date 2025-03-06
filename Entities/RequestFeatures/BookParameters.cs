f7d9dc3ac660bb2df6967993dc264ab0c5c69591
doğukan göktaş <dogukan.goktas@ogr.sakarya.edu.tr>
2025-03-06 15:13:27 +0300

    Filtering implementation

--- Entities/RequestFeatures/BookParameters.cs
+++ Entities/RequestFeatures/BookParameters.cs
@@ -2,5 +2,9 @@
 {
     public class BookParameters : RequestParameters
     {
+        public uint MinPrice { get; set; }
+        public uint MaxPrice { get; set; } = 1000; //kitapların max price 1000 olarak vermiştim 
+        public bool ValidPriceRange => MaxPrice > MinPrice;
+
     }
 }
--- Services/BookManager.cs
+++ Services/BookManager.cs
@@ -10,6 +10,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Entities.Exceptions.BadRequestException;
 
 namespace Services
 {
@@ -45,6 +46,10 @@ namespace Services
 
         public async Task<(IEnumerable<BookDto> books, MetaData metaData)> GetAllBooksAsync(BookParameters bookParameters, bool trackChanges)
         {
+            if (!bookParameters.ValidPriceRange)
+                throw new PriceOutOfRangeBadRequestException();
+
+
             var booksWithMetaData = await _manager.Book.GetAllBooksAsync(bookParameters, trackChanges);
             var booksDto = _mapper.Map<IEnumerable<BookDto>>(booksWithMetaData);
 
